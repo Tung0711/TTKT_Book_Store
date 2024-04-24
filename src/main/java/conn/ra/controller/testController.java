ae7bbd9cdc76fc3dbe6b9a610fc73053f3963d35
Pham Van Tung <Phamtungkp2002@gmail.com>
2024-04-24 15:44:46 +0700

    fix ui ux

--- src/main/java/conn/ra/controller/testController.java
+++ src/main/java/conn/ra/controller/testController.java
@@ -10,7 +10,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class testController {
     @GetMapping("/index")
-    public String SignIn() {
+    public String Index() {
         return "home/index";
+    }
+
+    @GetMapping("/about-us")
+    public String AboutUs() {
+        return "home/about-us";
+    }
+
+    @GetMapping("/books-detail")
+    public String BookDetail() {
+        return "home/books-detail";
+    }
+
+    @GetMapping("/books-grid-view")
+    public String BookGrid() {
+        return "home/books-grid-view";
+    }
+
+    @GetMapping("/books-list")
+    public String BookList() {
+        return "home/books-list";
+    }
+
+    @GetMapping("/contact-us")
+    public String ContactUs() {
+        return "home/contact-us";
+    }
+
+    @GetMapping("/my-profile")
+    public String MyProfile() {
+        return "home/my-profile";
+    }
+
+    @GetMapping("/shop-cart")
+    public String ShopCart() {
+        return "home/shop-cart";
+    }
+
+    @GetMapping("/shop-checkout")
+    public String ShopCheckout() {
+        return "home/shop-checkout";
+    }
+    @GetMapping("/wishlist")
+    public String Wishlist() {
+        return "home/wishlist";
     }
 }
